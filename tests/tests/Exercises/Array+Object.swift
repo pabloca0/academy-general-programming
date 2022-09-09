1dbf3e72b867ddd9b91fd968b0b1beaea38daf5c
Fernando Salom <Fdosalom@gmail.com>
2022-09-09 12:36:30 +0200

    Add object exercises

---
+++ tests/tests/Exercises/Array+Object.swift
@@ -0,0 +1,50 @@
+//
+//  Array+Object.swift
+//  tests
+//
+//  Created by Fernando Salom Carratala on 9/9/22.
+//
+
+import Foundation
+
+class ArrayAndObjectExercise {
+    /*:
+     ## Ejecicio 1
+     Crea una función que dados 2 strings los concatene y los devuelva
+    */
+    func test1(string1: String, string2: String) -> String {
+        return ""
+    }
+
+    /*:
+     ## Ejecicio 2
+     Crea una función que dada una frase sustituya todas las ocurrencias de un caracter por otro
+     */
+    func test2(text: String, search: Character, replace: Character) -> String {
+        return ""
+    }
+
+    /*:
+     ## Ejecicio 3
+     Crea una función que dado una frase borre el caracter que se le pasa si la contiene al principio o al final de la frase
+     */
+    func test3(text: String, character: Character ) -> String {
+        return ""
+    }
+
+    /*:
+     ## Ejecicio 4
+     Crea una función que dado un texto devuelva 2 veces las 2 primeras letras. Ejemplo "swift" devuelve "swsw"
+     */
+    func test4(text: String) -> String {
+        return ""
+    }
+
+    /*:
+     ## Ejecicio 5
+     Crea una función que dada una frase compruebe que los 2 primeros caracteres son iguales que los 2 últimos
+     */
+    func test5(text: String) -> Bool {
+        return false
+    }
+}
---
+++ tests/tests/Exercises/Object.swift
@@ -0,0 +1,37 @@
+//
+//  Object.swift
+//  tests
+//
+//  Created by Fernando Salom Carratala on 9/9/22.
+//
+
+import Foundation
+
+// TUS OBJETOS AQUI
+
+
+class ObjectExercise {
+    /*:
+     ## Ejecicio 1
+     Crea un función con los parámetros que necesites que cree y devuelva un objeto marca de coche con el nombre de la marca el año de creación y el país de origen
+    */
+    func test1() {
+    }
+
+    /*:
+     ## Ejecicio 2
+     Crea una función con los parámetros que necesites que cree y devuelva un objeto coche que tenga un nombre de modelo, fecha de fabricación, número de caballos y marca
+     */
+    func test2() {
+    }
+
+    /*:
+     ## Ejecicio 3
+     Vamos a crear un objeto que va a representar a un personaje con las siguiente caracteristicas
+     nombre, salud, ataque, defensa, raza (orco, humano, elfo) y funciones para reducir la salud por un número dado menos la defensa del personaje
+     Crea una función con los parámetros que necesites que cree un personaje y lo devuelva
+     */
+    func test3() {
+    }
+}
+
