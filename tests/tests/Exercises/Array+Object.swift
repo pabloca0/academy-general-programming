7b923a18958697a77da1cb636c4638750e703fac
Fernando Salom <Fdosalom@gmail.com>
2022-09-12 15:38:43 +0200

    Add new exercise

--- tests/tests/Exercises/Array+Object.swift
+++ tests/tests/Exercises/Array+Object.swift
@@ -10,41 +10,9 @@ import Foundation
 class ArrayAndObjectExercise {
     /*:
      ## Ejecicio 1
-     Crea una función que dados 2 strings los concatene y los devuelva
+     Crea las clases necesarias para definir clases y alumnos y añade valores de ejemplo para la clase
     */
-    func test1(string1: String, string2: String) -> String {
-        return ""
+    func test1() {
     }
 
-    /*:
-     ## Ejecicio 2
-     Crea una función que dada una frase sustituya todas las ocurrencias de un caracter por otro
-     */
-    func test2(text: String, search: Character, replace: Character) -> String {
-        return ""
-    }
-
-    /*:
-     ## Ejecicio 3
-     Crea una función que dado una frase borre el caracter que se le pasa si la contiene al principio o al final de la frase
-     */
-    func test3(text: String, character: Character ) -> String {
-        return ""
-    }
-
-    /*:
-     ## Ejecicio 4
-     Crea una función que dado un texto devuelva 2 veces las 2 primeras letras. Ejemplo "swift" devuelve "swsw"
-     */
-    func test4(text: String) -> String {
-        return ""
-    }
-
-    /*:
-     ## Ejecicio 5
-     Crea una función que dada una frase compruebe que los 2 primeros caracteres son iguales que los 2 últimos
-     */
-    func test5(text: String) -> Bool {
-        return false
-    }
 }
