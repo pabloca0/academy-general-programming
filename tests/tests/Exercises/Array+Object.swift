597254b71bbcb58aaef8532da0feb4ce73c1a76f
Fernando Salom <Fdosalom@gmail.com>
2022-09-12 20:46:16 +0200

    Add new exercises for array and classes

--- tests/tests/Exercises/Array+Object.swift
+++ tests/tests/Exercises/Array+Object.swift
@@ -10,9 +10,33 @@ import Foundation
 class ArrayAndObjectExercise {
     /*:
      ## Ejecicio 1
-     Crea las clases necesarias para definir clases y alumnos y añade valores de ejemplo para la clase
+     Crea las clases necesarias para definir clases y alumnos y añade. Crea 1 array con 2 clases con 10 alumnos cada una con valores de ejemplo
     */
     func test1() {
     }
 
+
+    /*:
+     ## Ejecicio 2
+     Imprime por consola todos los alumnos cuyo nombre contiene "a" en cada una de las clases
+    */
+    func test2() {
+
+    }
+
+    /*:
+     ## Ejecicio 3
+     Añade un atributo nuevo a la clase que sea isApproved y otra que sea isProgressing e imprime todos los alumnos suspendidos que no esten progresando de cada clase
+    */
+    func test3() {
+
+    }
+
+    /*:
+     ## Ejecicio 4
+     Añade un atributo nuevo a la clase que sea needHelp que sera true cuando isApproved e isProgressing sean false. Este atributo se debe autocalcular al iniciar la clase o cambiar el valor de isApproved o isProgressing a false e imprime todos los alumnos suspendidos de cada clase que necesitan ayuda
+    */
+    func test4() {
+
+    }
 }
