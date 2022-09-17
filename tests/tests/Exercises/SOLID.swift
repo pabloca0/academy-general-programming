e1774de98f5bc70852681ed44031e718ce896bc7
Fernando Salom <Fdosalom@gmail.com>
2022-09-17 11:58:14 +0200

    Update description SOLID principles

--- tests/tests/Exercises/SOLID.swift
+++ tests/tests/Exercises/SOLID.swift
@@ -9,23 +9,26 @@ import Foundation
 
 /*:
  ## Ejecicio 1
- Obten un array nuevo de alumnos suspendidos de todas las clases
+
+
+ Ejemplos SOLID
+
+ Single Responsibility Principle
+ It states that every module should have only one responsibility and reason to change. SRP starts with small concrete and specific cases such as a class and/or an object having only one purpose and being used only for one thing. This principle helps you to keep your classes as clean as possible.
+
+ Open/Closed Principle
+ In simple term, Open for extension but closed for modification.
+
+ Open for extension: You should be able to extend or change the behaviors of a class without efforts.
+ Closed for modification: You must extend a class without changing the implementation.
+
+ Liskov Substitution Principle
+ Functions that use pointers or references to base classes must be able to use objects of derived classes without knowing it.
+
+ Interface Segregation
+ It states that clients should not be forced to implement interfaces they don’t use. Instead of one fat interface, many small interfaces are preferred based on groups of methods, each one serving one submodule
+
+ Dependency Inversion
+ High-level modules should not depend on low-level modules both should depend on Abstractions. (Abstractions should not depend upon details. Details should depend upon abstractions)
+ 
 */
-public class NewspaperTeam {
-    func reviewArticle() {
-        // This function checks the article from the author.
-    }
-    func checkTypos() {
-        // This function checks for typos.
-    }
-    func toRevise() -> Bool {
-        // This function tells whether to revise the article according to the situation.
-        return false
-    }
-    func checkPublishDate() {
-        // This function checks the publication date of the article.
-    }
-    func checkPublishWay() {
-        // This function is controlling the publishing method.
-    }
-}
